910b0b0765b2a2b8e247fdb7589bee8c08cd8222
Jatin Kamboj <47236394+MJK618@users.noreply.github.com>
2021-10-30 15:05:43 +0530

    Hour 1 Complete

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # 100HoursOfGo
 100 Hours Of Golang
+
+# [Hour 1](hour1.go)
+    Started off with History, Need and Introduction of Golang
+    
+  
--- hour1.go
+++ hour1.go
@@ -3,6 +3,7 @@ package _100HoursOfGo
 import (
 	"fmt"
 )
+//Resource: https://app.pluralsight.com/library/courses/go-big-picture
 func hour1 () {
 	fmt.Println("Starting Hour 1 From History and Background")
 }
