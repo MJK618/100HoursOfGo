a30ab7d09c7223088fa228f9a4924a76ee5117b3
Jatin Kamboj <47236394+MJK618@users.noreply.github.com>
2021-11-06 19:36:25 +0530

    Hour 10 Completed

--- hour10.go
+++ hour10.go
@@ -16,6 +16,11 @@ var y = 43
 // and nil for pointers, functions, interfaces, slices, channels, and maps.
 var z int
 var zeta = 21
+
+var a int
+var b string = "James Bond"
+
+
 func hour10() {
 	// short declaration operator
 	// DECLARE a variable and ASSIGN a VALUE (of a certain TYPE)
@@ -32,6 +37,20 @@ func hour10() {
 	xeta = 50
 	fmt.Println(xeta)
 	fmt.Println(zeta)
+
+
+	fmt.Printf("%v\n", a)
+	fmt.Printf("%v\n", b)
+	fmt.Printf("%#v\n", a)
+	fmt.Printf("%#v\n", b)
+	fmt.Printf("%T\n", a)
+	fmt.Printf("%T\n", b)
+	fmt.Printf("%T\t%T\n", a, b)
+
+	s := fmt.Sprint(a, " something more ", b)
+	fmt.Println(s)
+	s2 := fmt.Sprintf("%v\t%T\t%T\n", "to pass in", a, b)
+	fmt.Println(s2)
 }
 
 func foo() {
