9849aa7f435f27f0f09091b85fea24aa09647fa2
Jatin Kamboj <47236394+MJK618@users.noreply.github.com>
2021-11-07 23:26:26 +0530

    Hour 16 Completed

--- README.md
+++ README.md
@@ -45,3 +45,6 @@
 
 # [Hour 15](hour15.go)
     Read Golang Specs for Control Flow, For Loop, If else and switch case statements along with logical operators
+
+# [Hour 16](hour16.go)
+    Practised topics covered in Hour 15 attended a quiz for the same
---
+++ hour16.go
@@ -0,0 +1,91 @@
+package _100HoursOfGo
+
+import "fmt"
+
+func main() {
+	for i := 1; i <= 10000; i++ {
+		fmt.Print(i, ", ")
+		if i%25 == 0 {
+			fmt.Println()
+		}
+	}
+
+	for i := 65; i <= 90; i++ {
+		fmt.Printf("%v\n", i)
+		for j := 0; j < 3; j++ {
+			fmt.Printf("\t%#U\n", i)
+		}
+	}
+
+	i := 2012
+	for i <= 2021 {
+		fmt.Printf("%v\n", i)
+		i++
+
+	}
+
+
+	i := 2012
+	for {
+		if i > 2021 {
+			break
+		}
+		fmt.Printf("%v\n", i)
+		i++
+	}
+
+
+	i := 10
+	for {
+		if i > 100 {
+			break
+		}
+		fmt.Println(i % 4)
+		i++
+	}
+
+
+	x := 105
+	if x > 100 {
+		fmt.Println(">100")
+	} else if x < 0 {
+
+		fmt.Println("<0")
+	} else {
+		fmt.Println(">0 & <100")
+	}
+
+
+
+	switch {
+	case false:
+		fmt.Println("I am switch false")
+	case true:
+		fmt.Println("I am switch true")
+	}
+
+
+	favSport := "Football"
+	switch {
+	case favSport == "Football":
+		fmt.Println("I am Football lover")
+	case favSport == "Cricket":
+		fmt.Println("I am Cricket lover")
+	default:
+		fmt.Println("I am None lover")
+	}
+	switch favSport  {
+	case "Football":
+		fmt.Println("I am Football lover")
+	case "Cricket":
+		fmt.Println("I am Cricket lover")
+	default:
+		fmt.Println("I am None lover")
+	}
+
+	fmt.Println(true && true)
+	fmt.Println(true && false)
+	fmt.Println(true || true)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
